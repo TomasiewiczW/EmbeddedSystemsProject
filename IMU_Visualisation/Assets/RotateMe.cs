83a3bf9cf5e08c220c87525f17a04528cafca2bb
TomasiewiczW <wojtarek@vp.pl>
2020-11-05 08:53:47 +0100

    Unity3d visualisation
    + scene,
    + square sprite,
    + script for controlling the sprite with arrow keys

---
+++ IMU_Visualisation/Assets/RotateMe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class RotateMe: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.down);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.right);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.left);
+        }
+
+    }
+}
